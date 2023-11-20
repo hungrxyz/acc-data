0128db4fc3620666e36760f7bdcd9d79dbe18fa9
marko <marko@hungry.dev>
2023-11-21 00:00:57 +0100

    Adds parsing of all categories

--- Sources/Scraper.swift
+++ Sources/Scraper.swift
@@ -12,70 +12,98 @@ import SwiftSoup
 struct Scraper: AsyncParsableCommand {
 
     mutating func run() async throws {
-        let url = URL(string: "https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vTxqu45sTCT3CtenEvlvuD9onGnyqwqNTOf-KMok2b9SPQ_ckVkvO7vuDraTaApwaJmwercGWYB-eiA/pubhtml/sheet?headers=false&gid=1908651275")!
-        let (data, _) = try await URLSession.shared.data(from: url)
 
-        let string = String(decoding: data, as: UTF8.self)
-
-        let rows = try SwiftSoup
-            .parse(string).select("tbody")
-            .first()!.select("tr")
-            .dropFirst(2)
-
-        let entries = rows.reduce(into: [[String]]()) { partialResult, row in
-            let values = (try? row.select("td").compactMap { try? $0.text() }) ?? []
-
-            if values.isEmpty == false {
-                partialResult.append(values)
+        enum Category: String, CaseIterable {
+            case ja, jb, jc, c, d, b, a
+            
+            var gid: String {
+                switch self {
+                case .ja:
+                    return "1745581134"
+                case .jb:
+                    return "906167346"
+                case .jc:
+                    return "294852544"
+                case .c:
+                    return "1292329851"
+                case .d:
+                    return "944348773"
+                case .b:
+                    return "563556052"
+                case .a:
+                    return "1908651275"
+                }
             }
         }
 
-        let results = entries.reduce(into: [TableResult]()) { partialResult, entry in
-            var raw = entry
+        for category in Category.allCases {
 
-            let position = raw.removeFirst()
-            let id = raw.removeFirst()
+            let url = URL(string: "https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vTxqu45sTCT3CtenEvlvuD9onGnyqwqNTOf-KMok2b9SPQ_ckVkvO7vuDraTaApwaJmwercGWYB-eiA/pubhtml/sheet?headers=false&gid=\(category.gid)")!
+            let (data, _) = try await URLSession.shared.data(from: url)
 
-            guard id.isEmpty == false else { return }
+            let string = String(decoding: data, as: UTF8.self)
 
-            // Removing ID2
-            raw.removeFirst()
-            let name = raw.removeFirst()
-            let club = raw.removeFirst()
+            let rows = try SwiftSoup
+                .parse(string).select("tbody")
+                .first()!.select("tr")
+                .dropFirst(2)
 
-            guard let age = Int(raw.removeFirst()),
-                  let numberOfRaces = Int(raw.removeFirst()),
-                  let totalPoints = Int(raw.removeFirst()) else {
-                return
+            let entries = rows.reduce(into: [[String]]()) { partialResult, row in
+                let values = (try? row.select("td").compactMap { try? $0.text() }) ?? []
+
+                if values.isEmpty == false {
+                    partialResult.append(values)
+                }
             }
 
-            let pointsPerRace = raw.map { Int($0) }
+            let results = entries.reduce(into: [TableResult]()) { partialResult, entry in
+                var raw = entry
 
-            let result = TableResult(
-                position: position,
-                id: id,
-                name: name,
-                club: club,
-                age: age,
-                numberOfRaces: numberOfRaces,
-                totalPoints: totalPoints,
-                pointsPerRace: pointsPerRace
-            )
+                let position = raw.removeFirst()
+                let id = raw.removeFirst()
 
-            partialResult.append(result)
+                guard id.isEmpty == false else { return }
+
+                // Removing ID2
+                raw.removeFirst()
+                let name = raw.removeFirst()
+                let club = raw.removeFirst()
+
+                guard let age = Int(raw.removeFirst()),
+                      let numberOfRaces = Int(raw.removeFirst()),
+                      let totalPoints = Int(raw.removeFirst()) else {
+                    return
+                }
+
+                let pointsPerRace = raw.map { Int($0) }
+
+                let result = TableResult(
+                    position: position,
+                    id: id,
+                    name: name,
+                    club: club,
+                    age: age,
+                    numberOfRaces: numberOfRaces,
+                    totalPoints: totalPoints,
+                    pointsPerRace: pointsPerRace
+                )
+
+                partialResult.append(result)
+            }
+
+            let fileManager = FileManager.default
+            let standingsURL = URL(fileURLWithPath: fileManager.currentDirectoryPath)
+                .appendingPathComponent("data")
+                .appendingPathComponent("2023")
+                .appendingPathComponent("standings")
+            try fileManager.createDirectory(at: standingsURL, withIntermediateDirectories: true)
+
+            let aURL = standingsURL.appendingPathComponent("\(category.rawValue).json")
+            let jsonData = try JSONEncoder().encode(results)
+
+            fileManager.createFile(atPath: aURL.path, contents: jsonData)
         }
 
-        let fileManager = FileManager.default
-        let standingsURL = URL(fileURLWithPath: fileManager.currentDirectoryPath)
-            .appendingPathComponent("data")
-            .appendingPathComponent("2023")
-            .appendingPathComponent("standings")
-        try fileManager.createDirectory(at: standingsURL, withIntermediateDirectories: true)
-
-        let aURL = standingsURL.appendingPathComponent("a.json")
-        let jsonData = try JSONEncoder().encode(results)
-        
-        fileManager.createFile(atPath: aURL.path, contents: jsonData)
     }
 
 }
